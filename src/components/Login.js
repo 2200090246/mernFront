9278cf8f323ecfec22bc86f637b9fd9c160888ca
2200090246 <148328823+2200090246@users.noreply.github.com>
2024-05-01 16:40:01 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState("z");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
